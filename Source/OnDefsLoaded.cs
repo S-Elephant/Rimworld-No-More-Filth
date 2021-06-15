2d787c4d5d866a1f067516147d7da2914d150c7b
Squirting Elephant <squirtingelephant@gmail.com>
2021-06-15 11:58:32 +0200

    Initial commit

---
+++ Source/OnDefsLoaded.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace NoMoreFilth
+{
+    [StaticConstructorOnStartup] // This attribute calls the static constructor AFTER the defs are loaded.
+    public class EGB_OnDefsLoaded
+    {
+        static EGB_OnDefsLoaded()
+        {
+            RemoveFilthFromAllTerrainDefs();
+        }
+
+        public static void RemoveFilthFromAllTerrainDefs()
+        {
+            foreach (TerrainDef terrainDef in DefDatabase<TerrainDef>.AllDefs)
+                terrainDef.filthAcceptanceMask = FilthSourceFlags.None;
+        }
+    }
+}
